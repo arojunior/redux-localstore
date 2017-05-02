b280936d0f099caf1370a7fb377f8e225b430b0a
Junior Oliveira <arojunior@gmail.com>
2017-05-02 18:10:33 -0300

    check if reducer keys is equal local reducer keys

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const isNull = value => value === 'undefined' || value === null
 
+const hasSameProps = (obj1, obj2) =>
+  Object.keys(obj1).every(prop => obj2.hasOwnProperty(prop))
+
 export const getLocalStore = () =>
   JSON.parse(localStorage.getItem('reduxStore'))
 
@@ -10,9 +13,14 @@ export const setLocalStore = store =>
 
 export const getState = () => (!isNull(getLocalStore()) ? getLocalStore() : {})
 
-export const defineState = (defaultState, reducer) =>
-  (getState().hasOwnProperty(reducer) ? getState()[reducer] : defaultState)
-
-export const resetState = () => localStorage.removeItem('reduxStore')
+export const defineState = (defaultState, reducer) => {
+  if (getState().hasOwnProperty(reducer)) {
+    const localReducer = getState()[reducer]
+    return hasSameProps(defaultState, localReducer)
+      ? localReducer
+      : defaultState
+  }
+  return defaultState
+}
 
 export default store => store.subscribe(() => setLocalStore(store))
